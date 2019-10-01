6f3b7e7528093512f2f6bfa68896f61ee63275c6
Cindy Halim <jcindyhalim@gmail.com>
2019-10-01 18:23:40 -0400

    Implement transition to create mode and back function

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,18 +4,38 @@ import "./styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+import useVisualMode from "../../hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
+  console.log(props);
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? (
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
         />
-      ) : (
-        <Empty />
+      )}
+      {mode === CREATE && (
+        <Form
+          name={props.student}
+          interviewers={[]}
+          interviewer={props.interviewer}
+          // onSave={props.onSave}
+          onCancel={() => back()}
+        />
       )}
     </article>
   );
